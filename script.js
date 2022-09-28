b06e364a000696b3b1b01b4975a5c6ce58de7a64
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-09-28 11:16:01 +0200

    tried js not succeeded

---
+++ script.js
@@ -0,0 +1,7 @@
+var el = document.getElementsByClassName('readMore');
+const hurensohn = function(event){
+    for (var i=0;i<el.length; i++) {
+    i.style.backgroundColor = "blue";
+}}
+
+el.addEventListener("click", hurensohn);
